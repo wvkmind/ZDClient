802d991eee6e43e7966cdd25cb8c868321c6ed85
wvkmind <wangwk@dehuinet.comm>
2018-12-17 18:33:23 +0800

    嗯

---
+++ Assets/Script/Animation/SpriteUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteUtils : MonoBehaviour {
+
+	Animator _ani;
+    public float m_speed = 0.5f;
+    // Use this for initialization
+    void Start () {
+        _ani = transform.GetComponent<Animator>();
+        _ani.speed = m_speed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Script/Game/Init.cs
+++ Assets/Script/Game/Init.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Init : MonoBehaviour {
 
 	void Start () {
 		NetWork.ConnectGate();
-		
 		//Register.In("test2","aklsdjfkla",data =>{Debug.Log("Register:"+data.ToString());});
 		Login.In("test2","aklsdjfkla",(data,error) =>{Debug.Log("Login:"+data.ToString()+":"+error);});
 	}
+	private void OnClick(){
+        if(start==false)
+		{
+			button_text.text = "停止";
+			start = true;
+		}
+		else
+		{
+			button_text.text = "开始";
+			start = false;
+		}
+    }
 	void Update () {
 		NetWork.Update();
 		NetEventDispatch.Update();
