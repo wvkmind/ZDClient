7c338e981c675282bf84d3fadc737594aa680d10
wvkmind <wangwk@dehuinet.comm>
2018-12-19 17:32:41 +0800

    登陆和错误框

---
+++ Assets/ErrorInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ErrorInfo : MonoBehaviour {
+
+	public UnityEngine.UI.Button ok;
+	public UnityEngine.UI.Text error_text;
+	public UnityEngine.GameObject obj;
+	public static string error_info = null;
+	// Use this for initialization
+	void Start () {
+		ok.onClick.AddListener(Exit);
+	}
+	void Exit(){
+		error_info = null;
+		obj.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(error_info!=null){
+			obj.SetActive(true);
+			error_text.text = error_info;
+		}
+	}
+}
---
+++ Assets/LoginUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginUI : MonoBehaviour {
+
+	public UnityEngine.UI.InputField account;
+	public UnityEngine.UI.InputField password;
+	public UnityEngine.UI.Button login;
+	// Use this for initialization
+	void Start () {
+		login.onClick.AddListener(LoginFunction);
+	}
+
+	void LoginFunction(){
+		Login.In(account.text,password.text,(data,error) =>{
+			if(error!=null&&!error.Equals(""))
+				ErrorInfo.error_info = error;
+			else
+				ErrorInfo.error_info = "登陆成功";
+		});
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
