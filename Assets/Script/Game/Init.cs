d44b3900e82012fe872bebc84400f1e696bc1f08
wvkmind <wangwk@dehuinet.comm>
2018-12-19 10:14:46 +0800

    开始的scene更名

--- Assets/Script/Game/Init.cs
+++ Assets/Script/Game/Init.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class Init : MonoBehaviour {
 
 	public UnityEngine.Object game_data;
-
+	
 	void Start () {
 		GameObject.DontDestroyOnLoad(game_data);
 		NetWork.ConnectGate();
