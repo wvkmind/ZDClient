390c4f6cf5e710be2233494668d5ea91a2216c90
wvkmind <wangwk@dehuinet.comm>
2018-12-19 09:51:10 +0800

    保证游戏管理不会因为sence改变而销毁

--- Assets/Script/Game/Init.cs
+++ Assets/Script/Game/Init.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Init : MonoBehaviour {
 
+	public UnityEngine.Object game_data;
+
 	void Start () {
+		GameObject.DontDestroyOnLoad(game_data);
 		NetWork.ConnectGate();
-		//Register.In("test2","aklsdjfkla",data =>{Debug.Log("Register:"+data.ToString());});
-		// Login.In("test2","aklsdjfkla",(data,error) =>{
-		// 	Debug.Log("Login:"+data.ToString()+":"+error);
-		// });
 	}
 	void Update () {
 		NetWork.Update();
 		NetEventDispatch.Update();
 	}
 	void OnDestory() {
-		//[TODO] 暂时放这里，真正的退出是游戏退出
 		NetWork.OnDestory();
 	}
 }
