de66936cf918a8f6da6b539873ab359d3127cd8c
wvkmind <wangwk@dehuinet.comm>
2019-01-08 10:01:58 +0800

    重回github！！！

---
+++ Assets/Script/Utils/GameCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour {
+    private int screen_width = 0;
+    private int screen_height = 0;
+    void Start () {
+        Debug.Log(Screen.width+"x"+Screen.height);
+    }
+    void Update () {
+        if(screen_width!=Screen.width||screen_height!=Screen.height){
+            screen_width = Screen.width;
+            screen_height = Screen.height;
+        }
+    }
+}
