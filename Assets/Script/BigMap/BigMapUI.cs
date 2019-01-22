9d3463c79bf0d1de9c4badafcdc4216e92214d92
wvkmind <wangwk@dehuinet.comm>
2019-01-22 13:49:42 +0800

    增加BigMapUI

---
+++ Assets/Script/BigMap/BigMapUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigMapUI : MonoBehaviour
+{
+    public UnityEngine.GameObject node;
+    private bool is_begen_touch = false;
+    private bool is_move = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount == 1)
+        {
+            if(Input.touches[0].phase == TouchPhase.Began)
+            {
+                is_begen_touch = true;
+            }
+            else
+            {
+                if(Input.touches[0].phase == TouchPhase.Moved)
+                {
+                    if(is_begen_touch)
+                    {
+                        float ch = node.transform.position.x+Input.touches[0].deltaPosition.x;
+                        if(ch<=0&&ch>=-1480)
+                            node.transform.position = new Vector3(ch,node.transform.position.y,node.transform.position.z);
+                    }
+                    is_move = true;
+                }
+                else
+                {
+                    is_move = false;
+                }
+                is_begen_touch = false;
+            }
+        }
+    }
+}
