bf0eb0c792bb0175e70afdbeae69f5c7f4f77203
wvkmind <wangwk@dehuinet.comm>
2019-01-24 11:33:46 +0800

    创建房间

--- Assets/Script/Labby/CreateRoomUI.cs
+++ Assets/Script/Labby/CreateRoomUI.cs
@@ -7,11 +7,42 @@ public class CreateRoomUI : MonoBehaviour
     public UnityEngine.UI.Button _createRoom;
     public UnityEngine.UI.Button _exit;
     public UnityEngine.UI.Button [] select_list;
+    public UnityEngine.UI.InputField room_name;
+    public UnityEngine.UI.InputField password;
     private int sellect_index = -1;
     // Start is called before the first frame update
     void Start()
     {
-       // _createRoom.onClick.AddListener();
+        _createRoom.onClick.AddListener(()=>{
+            string room_name_str = room_name.text;
+            if(room_name_str == ""){
+                ErrorInfo.CreateUI("请输入房间名字");
+                return;
+            }
+            string passrod_str = password.text;
+            string map_name = Map.MapName[sellect_index];
+            NetEventDispatch.RegisterEvent("create_room",data =>{
+                NetEventDispatch.UnRegisterEvent("create_room");
+                MsgPack.MessagePackObject tmp;
+                data.TryGetValue("status", out tmp);
+                int status = tmp.AsInt32();
+                if(status == 0){
+                    //TODO 进入房间
+                    ErrorInfo.CreateUI("创建房间成功");
+                }else{
+                    data.TryGetValue("error", out tmp);
+                    string error = tmp.AsString();
+                    ErrorInfo.CreateUI("房间创建失败:"+error);
+                }
+            });
+            Dictionary<string, object> dic = NetWork.getSendStart();
+            dic.Add("map_name",map_name);
+            dic.Add("room_name",room_name_str);
+            if(passrod_str=="")
+                dic.Add("password",passrod_str);
+            dic.Add("name", "create_room");
+            NetWork.Push(dic);
+        });
         _exit.onClick.AddListener(Cancel);
         for(int i=0;i<19;i++){
             int a = i;
