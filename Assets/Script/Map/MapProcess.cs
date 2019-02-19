d89fe15868c8fefea06b1ed4aff9de25612dbbd3
wvkmind <wangwk@dehuinet.comm>
2019-02-19 17:20:38 +0800

    走路改成单击

--- Assets/Script/Map/MapProcess.cs
+++ Assets/Script/Map/MapProcess.cs
@@ -7,11 +7,13 @@ using UnityEngine.EventSystems;
 public class MapProcess : MonoBehaviour
 {
     private bool touchEnd=true;
+    private bool moved = false;
     private float sc ;
     private float limit;
     private float unit; 
     private float proportion;
     public MapThings mapThings;
+    private Vector3 touchPositon;
     void Awake() {
         
     }
@@ -77,30 +79,46 @@ public class MapProcess : MonoBehaviour
         //for ios and android
         if(Input.touchCount == 1 )
         {
-            if(EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+            if(Input.touches[0].phase == TouchPhase.Began)
             {
-            }else{
-                if(Input.touches[0].phase == TouchPhase.Began)
+                if(EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
                 {
-                    if(touchEnd&&Input.touches[0].tapCount == 2)//双击走路
-                    {
-                        touchEnd = false;
-                        float real_screen_x = Input.touches[0].position.x;
-                        float real_screen_y = Input.touches[0].position.y;
-                        float design_x = (real_screen_x-Screen.width/2.0f)/proportion/100.0f;
-                        float design_y = (real_screen_y-Screen.height/2.0f)/proportion/100.0f;
-                        Vector3 pos = new Vector3(design_x,design_y,1.0f) - gameObject.transform.localPosition/3.2f;
-                        SendMyTouch(Init.me.transform.localPosition.x,Init.me.transform.localPosition.y,Init.me.GetComponent<RoleRender>().GetDirection(),pos.x,pos.y);
-                    }
-                }else if(touchEnd && Input.touches[0].phase == TouchPhase.Moved){
-                    Vector3 delta_pos =  Input.touches[0].deltaPosition;
-                    if(Mathf.Abs(transform.position.x+delta_pos.x/sc)<limit)
-                        transform.position = new Vector3(transform.position.x+delta_pos.x/sc,transform.position.y,transform.position.z);
+                    
                 }
-                else if(Input.touches[0].phase == TouchPhase.Ended||Input.touches[0].phase == TouchPhase.Canceled){
-                    touchEnd = true;
+                else{
+                    touchPositon = Input.touches[0].position;
+                    touchEnd = false;
+                }
+            }else if(Input.touches[0].phase == TouchPhase.Moved){
+                if(EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+                {
+                    
+                }
+                else
+                {
+                    if(Mathf.Abs(touchPositon.x-Input.touches[0].position.x)>15&&Mathf.Abs(touchPositon.y-Input.touches[0].position.y)>15||moved)
+                    {
+                        moved = true;
+                        Vector3 delta_pos =  Input.touches[0].deltaPosition;
+                        if(Mathf.Abs(transform.position.x+delta_pos.x/sc)<limit)
+                            transform.position = new Vector3(transform.position.x+delta_pos.x/sc,transform.position.y,transform.position.z);
+                    }
                 }
             }
+            else if(Input.touches[0].phase == TouchPhase.Ended||Input.touches[0].phase == TouchPhase.Canceled){
+                if(!touchEnd&&!moved)
+                {   
+                    float real_screen_x = Input.touches[0].position.x;
+                    float real_screen_y = Input.touches[0].position.y;
+                    float design_x = (real_screen_x-Screen.width/2.0f)/proportion/100.0f;
+                    float design_y = (real_screen_y-Screen.height/2.0f)/proportion/100.0f;
+                    Vector3 pos = new Vector3(design_x,design_y,1.0f) - gameObject.transform.localPosition/3.2f;
+                    SendMyTouch(Init.me.transform.localPosition.x,Init.me.transform.localPosition.y,Init.me.GetComponent<RoleRender>().GetDirection(),pos.x,pos.y);
+                }
+                touchEnd = true;
+                moved = false;
+            }
+            
         }
     }
     public static void SendMyTouch(float cur_x,float cur_y,int direction,float target_x,float tartge_y){
